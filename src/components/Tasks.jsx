33964c87c6cf1e5cda12c15e5e46345cbe4f6fd9
muthuselvam73 <muthuselvam73.bit@gmail.com>
2024-11-08 06:10:16 +0530

    updated

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import Button from "./Button";
-import BoardView from "./BoardView";
+
+import BoardView from "../components/BoardView";
 import { FaPlus } from "react-icons/fa";
+import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/modal";
+import { Input } from "postcss";
+import { Button, Checkbox } from "@nextui-org/react";
+import { IoMailUnread } from "react-icons/io5";
+import { IoMdLock } from "react-icons/io";
+import { Link } from "react-router-dom";
 export default function Tasks() {
+
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
   
   // console.log(tasks)
   return (
@@ -10,20 +18,62 @@ export default function Tasks() {
       <div className="flex items-center justify-between mb-4">
         <div className="font-bold text-xl">Tasks</div>
 
-        {/* {!status && ( */}
-        <Button
-          // onClick={() => setOpen(true)}
-          label="Create Task"
-          icon={
-            <div>
-              <FaPlus />
-            </div>
-          }
-          className="flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5"
-        />
-        
-        {/* )} */}
+        <Button onPress={onOpen} color="primary">Open Modal</Button>
+      <Modal 
+        isOpen={isOpen} 
+        onOpenChange={onOpenChange}
+        placement="top-center"
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalBody>
+                <Input
+                  autoFocus
+                  endContent={
+                    <IoMailUnread className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Email"
+                  placeholder="Enter your email"
+                  variant="bordered"
+                />
+                <Input
+                  endContent={
+                    <IoMdLock className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                  }
+                  label="Password"
+                  placeholder="Enter your password"
+                  type="password"
+                  variant="bordered"
+                />
+                <div className="flex py-2 px-1 justify-between">
+                  <Checkbox
+                    classNames={{
+                      label: "text-small",
+                    }}
+                  >
+                    Remember me
+                  </Checkbox>
+                  <Link color="primary" href="#" size="sm">
+                    Forgot password?
+                  </Link>
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="flat" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Sign in
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
       </div>
+     
 
       {/* <Tabs tabs={TABS} setSelected={setSelected}>
         {!status && (
